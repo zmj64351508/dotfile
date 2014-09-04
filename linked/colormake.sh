0fa024d412bb120805301fa676f3f227599fad6a
MageeZhou <MageeZhou@zhaoxin.com>
2014-09-04 11:13:52 +0800

    update colormake.sh

--- linked/colormake.sh
+++ linked/colormake.sh
@@ -6,7 +6,8 @@ pathpat="(.*[^/]*)+:[0-9]+"
 ccred=$(echo -e "\033[0;31m")
 ccyellow=$(echo -e "\033[0;33m")
 ccend=$(echo -e "\033[0m")
-$MAKE "$@" 2>&1 | sed -E -e "/[Ee]rror[: ]/ s%$pathpat%$ccred&$ccend%g" -e "/[Ww]arning[: ]/ s%$pathpat%$ccyellow&$ccend%g"
-return ${PIPESTATUS[0]}
-
+$MAKE "$@" 2>&1 | sed -E -e "
+    /[Ee]rror[: ]/ s%$pathpat%$ccred&$ccend%g
+    /[Ww]arning[: ]/ s%$pathpat%$ccyellow&$ccend%g
+"
 

841ede171854e399a3c775ba4c024d6dd4c55742
MageeZhou <MageeZhou@zhaoxin.com>
2014-09-29 09:56:08 +0800

    update dotfile.py to support more features

--- dotfile.py
+++ dotfile.py
@@ -1,82 +1,52 @@
 #! /usr/bin/python
 import os
 import sys, getopt
-import sqlite3
 
 ################# Configurations #########################
-backup_files = (
-    {"source": "~/.profile", "backup": "profile"},
-    {"source": "~/.bashrc",  "backup": "bashrc"},
-    {"source": "~/.vimrc",   "backup": "vimrc"},
-    {"source": "/usr/share/vim/vim74/syntax/c.vim",     "backup": "c.vim"},
-)
-
-linked_files = (
-    {"target": "~/bin/make", "backup": "colormake.sh"}, # color make
-    {"target": "~/bin/deploy", "backup": "deploy.sh"},  # deploy
-)
-
 backup_dir = os.path.expanduser("~/backup")
 linked_dir = backup_dir + "/" + "linked"
-cp = "cp -rf "
+cp = "cp -f "
 ln = "ln -s "
 mv = "mv "
-
+quiet = False
 ################# Script start ###########################
 def run_cmd(cmd):
-    print cmd
+    if not quiet:
+        print cmd
     os.system(cmd)
 
-def update_backup():
-    if not os.path.isdir(backup_dir):
-        print "Create %s" %(backup_dir)
-        os.makedirs(backup_dir)
-
-    print "Start Update..."
-    for files in backup_files:
-        cmd = cp + files["source"] + " " + backup_dir + "/" + files["backup"]
-        run_cmd(cmd)
-
-def restore():
-    print "Backup original files.."
-    for files in backup_files:
-        source_path = os.path.expanduser(files["source"])
-        # backup current file first
-        if os.path.exists(source_path):
-            run_cmd(cp + source_path + " " + source_path + ".dotbak")
-
-    print "Start Restore.."
-    for files in backup_files:
-        source_path = os.path.expanduser(files["source"])
-        # do restore
-        if(os.path.exists(backup_dir + "/" + files["backup"])):
-            run_cmd(cp + backup_dir + "/" + files["backup"] + " " + source_path)
-
-def link():
-    print "Start linking.."
-    for files in linked_files:
-        target_path = os.path.expanduser(files["target"])
-        if os.path.exists(target_path) or os.path.islink(target_path):
-            run_cmd(mv + target_path + " " + target_path + ".dotbak")
-        # automaticly make dirs is not exsisted
-        target_parent = os.path.dirname(target_path)
-        if not os.path.isdir(target_parent):
-            os.makedirs(target_parent)
-        run_cmd(ln + linked_dir + "/" + files["backup"] + " " + target_path)
-        
-def print_usage():
-    print "usage:"
+def print_common_opt_usage():
+    print "    -h --help  -- display help"
+    print "    -q --quiet -- do not display command message"
 
 def print_sub_cmds():
     print "sub commands: "
-    print "    u, update  -- update backup dir according to backup_files"
+    print "    u, update  -- update backup dir according to backupfiles"
     print "    r, restore -- restore backup dir to system"
-    print "    l, link    -- link files according to linked_files to system"
+    print "    l, link    -- link files according to linkedfiles to system"
+
+def print_update_usage():
+    print "usage: update -hq [files...]"
+    print "If no file specified, it will update all the files in backupfiles and linkedfiles"
+    print_common_opt_usage()
+
+def print_restore_usage():
+    print "usage: restore -hq [files...]"
+    print "If no file specified, it will restore all the files in backupfiles and linkedfiles"
+    print_common_opt_usage()
+
+def print_link_usage():
+    print "usage: link -hq [files...]"
+    print "If no file specified, it will link all the files in linkedfiles"
+    print_common_opt_usage()
 
 class BackupFile(object):
-    def __init__(self, source):
+    def __init__(self, source, backup):
         self.source = source
-        self.backup = self.make_backup(self.source)
+        if backup:
+            self.backup = backup
+        else:
+            self.backup = self.make_backup(self.source)
 
     def make_backup(self, source):
         backup = os.path.basename(source);
@@ -91,20 +61,36 @@ class BackupFiles(object):
     # return the list of all the backup files
     def __get_all_files(self):
         sources_list = []
-        sources = self.db.readlines()
-        for source in sources:
-            source = source.strip()
+        line = self.db.readlines()
+        line_num = 0
+        for curline in line:
+            line_num += 1
+            comment_split = curline.split("#", 1)
+            nocomment = comment_split[0].strip()
+            if not nocomment:
+                continue
+            nocomment_split = nocomment.split()
+            source = nocomment_split[0].strip()
+            if len(nocomment_split) == 2:
+                backup = nocomment_split[1].strip()
+            elif len(nocomment_split) > 2:
+                print "Error: ignore " + self.db_dir + ":%d" % line_num
+                continue
+            else:
+                backup = None
             if source:
-                sources_list.append(BackupFile(source))
+                sources_list.append(BackupFile(source, backup))
+            
         return sources_list
     
-    def do_backup(self, backup_file):
-        cmd = cp + backup_file.source + " " + backup_dir + "/" + backup_file.backup
+    def do_backup(self, backupfile):
+        source_path = os.path.expanduser(backupfile.source)
+        cmd = cp + source_path + " " + backup_dir + "/" + backupfile.backup
         run_cmd(cmd)
 
     def do_restore(self, backupfile):
         # backup current file first
-        source_path = backupfile.source
+        source_path = os.path.expanduser(backupfile.source)
         if os.path.exists(source_path):
             run_cmd(cp + source_path + " " + source_path + ".dotbak")
 
@@ -122,6 +108,12 @@ class BackupFiles(object):
         for backupfile in self.all_files:
             self.do_backup(backupfile)
 
+    def get_backupfile(self, name):
+        for backupfile in self.all_files:
+            if os.path.expanduser(backupfile.source) == os.path.expanduser(name):
+                return backupfile
+        return None
+
 class LinkedFiles(BackupFiles):
 
     def do_backup(self, backup_file):
@@ -137,14 +129,6 @@ class LinkedFiles(BackupFiles):
             os.makedirs(source_parent)
         run_cmd(ln + linked_dir + "/" + backupfile.backup + " " + source_path)
 
-#if __name__ == "__main__":
-#    backupfiles = BackupFiles(backup_dir + "/" + "backupfiles")
-#    linkedfiles = LinkedFiles(backup_dir + "/" + "linkedfiles")
-#    
-#    #backupfiles.do_all_backup()
-#    backupfiles.do_all_restore()
-#    #linkedfiles.do_all_backup()
-#    #linkedfiles.do_all_restore()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -152,7 +136,7 @@ if __name__ == "__main__":
         sys.exit()
 
     sub_cmd = sys.argv[1];
-    opts, args = getopt.getopt(sys.argv[2:], "h", ["help"])
+    opts, args = getopt.getopt(sys.argv[2:], "hq", ["help", "quiet"])
     do_update = False
     do_restore = False
     do_link = False
@@ -161,24 +145,67 @@ if __name__ == "__main__":
             do_update = True
         elif sub_cmd in ("r", "restore"):
             do_restore = True
-            do_link = True
         elif sub_cmd in ("l", "link"):
             do_link = True
+        else:
+            print_sub_cmds()
+            sys.exit()
+            
 
     for op, value in opts:
         if op in ("-h", "--help"):
-            print_usage()
+            if do_update:
+                print_update_usage()
+            elif do_restore:
+                print_restore_usage()
+            elif do_link:
+                print_link_usage()
             sys.exit()
+        if op in ("-q", "--quiet"):
+            quiet = True;
 
     backupfiles = BackupFiles(backup_dir + "/" + "backupfiles")
     linkedfiles = LinkedFiles(backup_dir + "/" + "linkedfiles")
 
-    if do_update:
-        backupfiles.do_all_backup()
+    files = []
+    files_count = 0
+    if len(args) != 0:
+        files = args
+        files_count = len(args)
 
-    if do_restore:
-        backupfiles.do_all_restore()
+    backup_list = []
+    linked_list = []
+    if files_count > 0:
+        for candidate in files:
+            backupfile = backupfiles.get_backupfile(candidate)
+            if not backupfile:
+                linkedfile = linkedfiles.get_backupfile(candidate)
 
-    if do_link:
-        linkedfiles.do_all_restore()
+            if backupfile:
+                backup_list.append(backupfile)
+            elif linkedfile:
+                linked_list.append(linkedfile)
+            else:
+                print "Warning: file \"" + candidate + "\" is not in backupfiles or linkedfiles, ignoring it"
+
+        for candidate in backup_list:
+            if do_update:
+                backupfiles.do_backup(candidate)
+            elif do_restore:
+                backupfiles.do_restore(candidate)
+        
+        for candidate in linked_list:
+            if do_restore or do_link:
+                linkedfiles.do_restore(candidate)
+
+    else:
+        if do_update:
+            backupfiles.do_all_backup()
+
+        elif do_restore:
+            backupfiles.do_all_restore()
+            linkedfiles.do_all_restore()
+
+        elif do_link:
+            linkedfiles.do_all_restore()
 

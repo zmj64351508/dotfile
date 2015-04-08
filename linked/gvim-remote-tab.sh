32cfb5213330b382b509b840dd0cc8a691756ac9
MageeZhou <MageeZhou@zhaoxin.com>
2015-04-08 11:29:14 +0800

    Fix some bugs in gvim-remote-tab.sh

--- linked/gvim-remote-tab.sh
+++ linked/gvim-remote-tab.sh
@@ -10,7 +10,7 @@ echo "Avalible servers:"
 # show avalible servers
 #gvim --serverlist | sed '$a\New gvim' | sed "=" | sed -r -e "N;s/([^\n]+)\n(^)/\1: \2/" 
 #gvim --serverlist | sort | sed '$a\n: New gvim' | awk '$0=""NR-1": "$0'
-gvim --serverlist | sort | sed -e "s/^.*[^0-9]$/0: \0/" -e '$a\n: New gvim' | sed -r -e "s/^.*([0-9]+)$/\1: \0/"
+gvim --serverlist | sed -r -e "s/^[^0-9]*([0-9]+)$/\1: \0/" | sort -g | sed -e "s/^.*[^0-9]$/0: \0/" -e '$a\n: New gvim'
 
 new_server=false
 # validate input
@@ -18,7 +18,7 @@ while true; do
 	echo -n "Choose: "
 	read chosen
 	#new_server_index=`expr $server_num + 1`
-	if [ $chosen = 'n' ]; then
+	if [[ $chosen = 'n' || $chosen = '' ]]; then
 		new_server=true
 		break;
 	elif [[ $chosen -lt 0 ]]; then
@@ -36,8 +36,13 @@ done
 if [[ $new_server = true ]]; then
 	gvim $@
 else
+	if [[ $@ = '' ]]; then
+		file_name=new_file
+	else
+		file_name=$@
+	fi
 	# attach to server
 	server=${server_base_name}${server_index}
-	gvim --servername $server --remote-tab-silent $@
+	gvim --servername $server --remote-tab-silent $file_name
 fi
 
